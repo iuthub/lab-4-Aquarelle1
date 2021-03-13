a4ebed004a1f51f24f1d9d62ba817870cc43fbbd
Aquarelle1 <45044528+Aquarelle1@users.noreply.github.com>
2021-03-14 00:47:48 +0500

    feat:IP_Lab4

---
+++ webpage/playlist_songs.php
@@ -0,0 +1,15 @@
+<?php
+
+    $playlist = $_REQUEST["playlist"];
+    $lines = file("songs/".$playlist);
+
+    foreach ($lines as $line) {
+ ?>
+
+    <li class="mp3item">
+        <?= $line ?>
+    </li>
+
+<?php
+    }
+    ?>
